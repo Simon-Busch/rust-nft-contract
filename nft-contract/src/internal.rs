7115e3bb3bd885d52123defeca8d28e992fe5fb8
simonbusch <Simon.busch@live.fr>
2022-03-07 14:12:43 +0100

    fix function call, carry on Near blockchain tutorial

--- nft-contract/src/internal.rs
+++ nft-contract/src/internal.rs
@@ -2,21 +2,9 @@ use crate::*;
 use near_sdk::{CryptoHash};
 use std::mem::size_of;
 
-//used to generate a unique prefix in our storage collections (this is to avoid data collisions)
-pub(crate) fn hash_account_id(account_id: &AccountId) -> CryptoHash {
-    //get the default hash
-    let mut hash = CryptoHash::default();
-    //we hash the account ID and return it
-    hash.copy_from_slice(&env::sha256(account_id.as_bytes()));
-    hash
-}
-
-//Assert that the user has attached at least 1 yoctoNEAR (for security reasons and to pay for storage)
-pub(crate) fn assert_at_least_one_yocto() {
-    assert!(
-        env::attached_deposit() >= 1,
-        "Requires attached deposit of at least 1 yoctoNEAR",
-    )
+//convert the royalty percentage and amount to pay into a payout (U128)
+pub(crate) fn royalty_to_payout(royalty_percentage: u32, amount_to_pay: Balance) -> U128 {
+    U128(royalty_percentage as u128 * amount_to_pay / 10_000u128)
 }
 
 //calculate how many bytes the account ID is taking up
@@ -29,10 +17,10 @@ pub(crate) fn bytes_for_approved_account_id(account_id: &AccountId) -> u64 {
 pub(crate) fn refund_approved_account_ids_iter<'a, I>(
     account_id: AccountId,
     approved_account_ids: I, //the approved account IDs must be passed in as an iterator
-    ) -> Promise
-    where
-        I: Iterator<Item = &'a AccountId>,
-    {
+) -> Promise
+where
+    I: Iterator<Item = &'a AccountId>,
+{
     //get the storage total by going through and summing all the bytes for each approved account IDs
     let storage_released: u64 = approved_account_ids.map(bytes_for_approved_account_id).sum();
     //transfer the account the storage that is released
@@ -43,9 +31,35 @@ pub(crate) fn refund_approved_account_ids_iter<'a, I>(
 pub(crate) fn refund_approved_account_ids(
     account_id: AccountId,
     approved_account_ids: &HashMap<AccountId, u64>,
-    ) -> Promise {
+) -> Promise {
     //call the refund_approved_account_ids_iter with the approved account IDs as keys
     refund_approved_account_ids_iter(account_id, approved_account_ids.keys())
+}
+
+//used to generate a unique prefix in our storage collections (this is to avoid data collisions)
+pub(crate) fn hash_account_id(account_id: &AccountId) -> CryptoHash {
+    //get the default hash
+    let mut hash = CryptoHash::default();
+    //we hash the account ID and return it
+    hash.copy_from_slice(&env::sha256(account_id.as_bytes()));
+    hash
+}
+
+//used to make sure the user attached exactly 1 yoctoNEAR
+pub(crate) fn assert_one_yocto() {
+    assert_eq!(
+        env::attached_deposit(),
+        1,
+        "Requires attached deposit of exactly 1 yoctoNEAR",
+    )
+}
+
+//Assert that the user has attached at least 1 yoctoNEAR (for security reasons and to pay for storage)
+pub(crate) fn assert_at_least_one_yocto() {
+    assert!(
+        env::attached_deposit() >= 1,
+        "Requires attached deposit of at least 1 yoctoNEAR",
+    )
 }
 
 //refund the initial deposit based on the amount of storage that was used up
@@ -69,15 +83,6 @@ pub(crate) fn refund_deposit(storage_used: u64) {
     if refund > 1 {
         Promise::new(env::predecessor_account_id()).transfer(refund);
     }
-}
-
-//used to make sure the user attached exactly 1 yoctoNEAR
-pub(crate) fn assert_one_yocto() {
-    assert_eq!(
-        env::attached_deposit(),
-        1,
-        "Requires attached deposit of exactly 1 yoctoNEAR",
-    )
 }
 
 impl Contract {
@@ -146,29 +151,29 @@ impl Contract {
         let token = self.tokens_by_id.get(token_id).expect("No token");
 
         //if the sender doesn't equal the owner, we check if the sender is in the approval list
-    if sender_id != &token.owner_id {
-    //if the token's approved account IDs doesn't contain the sender, we panic
-    if !token.approved_account_ids.contains_key(sender_id) {
-        env::panic_str("Unauthorized");
-    }
+		if sender_id != &token.owner_id {
+			//if the token's approved account IDs doesn't contain the sender, we panic
+			if !token.approved_account_ids.contains_key(sender_id) {
+				env::panic_str("Unauthorized");
+			}
 
-    // If they included an approval_id, check if the sender's actual approval_id is the same as the one included
-    if let Some(enforced_approval_id) = approval_id {
+			// If they included an approval_id, check if the sender's actual approval_id is the same as the one included
+			if let Some(enforced_approval_id) = approval_id {
                 //get the actual approval ID
-        let actual_approval_id = token
-            .approved_account_ids
-            .get(sender_id)
+				let actual_approval_id = token
+					.approved_account_ids
+					.get(sender_id)
                     //if the sender isn't in the map, we panic
-            .expect("Sender is not approved account");
+					.expect("Sender is not approved account");
 
                 //make sure that the actual approval ID is the same as the one provided
                 assert_eq!(
-            actual_approval_id, &enforced_approval_id,
-            "The actual approval_id {} is different from the given approval_id {}",
-            actual_approval_id, enforced_approval_id,
-        );
-    }
-    }
+					actual_approval_id, &enforced_approval_id,
+					"The actual approval_id {} is different from the given approval_id {}",
+					actual_approval_id, enforced_approval_id,
+				);
+			}
+		}
 
         //we make sure that the sender isn't sending the token to themselves
         assert_ne!(
@@ -187,6 +192,8 @@ impl Contract {
             //reset the approval account IDs
             approved_account_ids: Default::default(),
             next_approval_id: token.next_approval_id,
+            //we copy over the royalties from the previous token
+            royalty: token.royalty.clone(),
         };
         //insert that new token into the tokens_by_id, replacing the old entry 
         self.tokens_by_id.insert(token_id, &new_token);
@@ -199,4 +206,4 @@ impl Contract {
         //return the preivous token object that was transferred.
         token
     }
-}
+} 
